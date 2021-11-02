218800742929a5209cf7d3a9c6689af5448543fe
Bernardo Meurer <bernardo@meurer.org>
2021-11-02 00:33:43 -0700

    nixos-option: pin to nix_2_3

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -32420,7 +32420,7 @@ with pkgs;
 
   nix-linter = haskell.lib.justStaticExecutables (haskellPackages.nix-linter);
 
-  nixos-option = callPackage ../tools/nix/nixos-option { };
+  nixos-option = callPackage ../tools/nix/nixos-option { nix = nix_2_3; };
 
   nix-pin = callPackage ../tools/package-management/nix-pin { };
 
